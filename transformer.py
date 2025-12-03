26e61bab6add627cb8b74bb9fec148fdddc838d7
James Cabot <jwcabot@mit.edu>
2025-12-02 21:54:02 -0500

    Add Transformer skeleton code, incomplete

---
+++ transformer.py
@@ -0,0 +1,9 @@
+############################
+# Lucas put your code here #
+############################
+class StockTransformer:
+    def __init__(self):
+        pass # Edit this function
+
+    def forward(self, x):
+        pass # Edit this function
